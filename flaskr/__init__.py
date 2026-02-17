ffc7c632b994a1d9ce0fea297fc108b35eae7696
DigDeepDug <elliotbowers2@gmail.com>
2026-02-17 11:14:16 -0800

    Removing blog functionality, adding radio functionality

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -31,8 +31,8 @@ def create_app(test_config=None):
     from . import auth
     app.register_blueprint(auth.bp)
 
-    from . import blog
-    app.register_blueprint(blog.bp)
+    from . import radio
+    app.register_blueprint(radio.bp)
     app.add_url_rule('/', endpoint='index')
 
     
